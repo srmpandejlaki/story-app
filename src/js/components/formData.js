2eea5e4b5927434680b3c8958cbe9bc8bafa19e8
Mutiara <mutiarapandejlaki27@gmail.com>
2025-02-04 21:44:45 +0800

    tambah sedikit di bagian form

--- src/js/components/formData.js
+++ src/js/components/formData.js
@@ -1,6 +1,18 @@
 import { LitElement, html, css } from 'lit';
  
 class formData extends LitElement {
+  static properties = {
+    minTitleLength: {
+      type: Number,
+    },
+    freeze: {
+      type: Boolean,
+    },
+    withPlaceholder: {
+      type: Boolean,
+    },
+  };
+
   static styles = css`
     form > div {
       margin-block-end: 8px;
@@ -11,15 +23,21 @@ class formData extends LitElement {
       display: block;
     }
   `;
+
+  constructor() {
+    super();
+ 
+    this.minTitleLength = 6;
+  }
  
   render() {
     return html`
       <form action="/story/add" method="POST">
             <label for="title">Judul:</label>
-            <input type="text" id="title" name="title" required>
+            <input type="text" id="title" name="title" minlength=${this.minTitleLength} ?readonly=${this.freeze} placeholder=${this.withPlaceholder ? 'Masukkan judul' : nothing} required>
             <br><br>
             <label for="description">Deskripsi:</label>
-            <textarea id="description" name="description" required></textarea>
+            <textarea id="description" name="description" ?readonly=${this.freeze} placeholder=${this.withPlaceholder ? 'Masukkan deskripsi' : nothing} required></textarea>
             <input type="submit" value="Submit">
         </form>
     `;
