48bbe999cc7240cbedc97bf0dd34514933df773a
Mutiara <mutiarapandejlaki27@gmail.com>
2025-02-13 14:35:28 +0800

    styling sedikit

--- src/js/components/socialDev.js
+++ src/js/components/socialDev.js
@@ -10,9 +10,11 @@ class socialDev extends LitElement {
       <article class="social">
         <h2>Sosial Media</h2>
         <p>Email: mutiarapandejlaki47@gmail.com</p>
-        <a href="https://github.com/srmpandejlaki">GitHub</a>
-        <a href="https://www.linkedin.com/in/sesilia-pandejlaki/">LinkedIn</a>
-        <a href="https://www.instagram.com/mutiarapandejlaki27/?hl=en">Instagram</a>
+        <div class="link">
+          <a href="https://github.com/srmpandejlaki">GitHub</a>
+          <a href="https://www.linkedin.com/in/sesilia-pandejlaki/">LinkedIn</a>
+          <a href="https://www.instagram.com/mutiarapandejlaki27/?hl=en">Instagram</a>
+        </div>
       </article>
     `;
   }
